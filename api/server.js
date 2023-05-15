9af15469748919cf1f10bc283a38b2ba65656f66
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-05-15 17:46:58 +0300

    post kısmı yapıldı.

--- api/server.js
+++ api/server.js
@@ -4,9 +4,31 @@
 
 const express = require("express");
 const server = express();
-
+const Users = require("./users/model");
 server.get("/", (req, res) => {
   res.send("Server is up and running!...");
+});
+
+// server ayağa kaldırıldı.
+server.use(express.json());
+
+//POST
+server.post("/api/users", (req, res) => {
+  let user = req.body;
+  if (!user.name || !user.bio) {
+    res
+      .status(400)
+      .json({ message: "Lütfen kullanıcı için bir name ve bio sağlayın" });
+  } else {
+    Users.insert(user)
+      .then((newUser) => {
+        res.status(201).json(newUser);
+      })
+      .catch((err) => {
+        res.status(500);
+        json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+      });
+  }
 });
 
 module.exports = server;
